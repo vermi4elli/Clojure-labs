a48d40ebfae6a8274a755d21b0228e33503674b8
vermi4elli <nikita.molodtsov21@gmail.com>
2020-02-18 01:06:18 +0200

    Created a bad realization of the first task of the lab2

--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -6,3 +6,13 @@
   [& args]
   (println "Hello, World!"))
 
+(defn GenerateSequenceDumbMethod
+  [x y z]
+  (if (< y (- z 1)) (let [value (* -1 (* 2 (peek x)))
+                     counter (+ y 1)]
+                    (GenerateSequenceDumbMethod (conj x value) counter z)
+                 )
+               (println x)))
+
+(GenerateSequenceDumbMethod [1] 0 4)
+
