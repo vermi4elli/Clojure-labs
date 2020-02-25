f6c1ea0f92273d08954732925e3e8ed1f239fdd0
vermi4elli <nikita.molodtsov21@gmail.com>
2020-02-25 16:00:02 +0200

    Finished initial parsing of the files, moving onto writing the general parsing function and calling in from the terminal

--- lab3/src/lab3/core.clj
+++ lab3/src/lab3/core.clj
@@ -14,25 +14,45 @@
 
 ; ========= First task - parsing the first csv files ==========
 
-(def map_zal-skl9 (rest (vec (with-open [reader (io/reader "../data files/map_zal-skl9.csv")]
-  ; due to the fact that read-csv produces lazy-seq,
-  ; we use the 'doall' function that forces all lazy-seq
-  (doall
-    (csv/read-csv reader))))))
+;1.1) ----------- parsing the FIRST MANDATORY file 'mp-posts_full'
+(def mp-posts_full (vec (rest (vec (with-open [reader (io/reader "../data files/mp-posts_full.csv")]
+                                     (doall
+                                       (csv/read-csv reader)))))))
 
-(def mp-posts_full (with-open [reader (io/reader "../data files/mp-posts_full.csv")]
-                    (doall
-                      (csv/read-csv reader))))
+;1.2) ----------- parsing the SECOND MANDATORY file 'map_zal-skl9'
+(def map_zal-skl9 (vec (rest (vec (with-open [reader (io/reader "../data files/map_zal-skl9.csv")]
+                                    ; due to the fact that read-csv produces lazy-seq,
+                                    ; we use the 'doall' function that forces all lazy-seq
+                                    (doall
+                                      (csv/read-csv reader)))))))
 
+;1.3) ----------- parsing the THIRD MANDATORY file 'plenary_register_mps-skl9'
+(def plenary_register_mps-skl9_raw (vec (doall
+                 (line-seq
+                   (io/reader "../data files/plenary_register_mps-skl9.tsv")))))
+
+;writing the final structure into 'plenary_register_mps-skl9_final'
+(def plenary_register_mps-skl9_final (vec (for [string plenary_register_mps-skl9_raw]
+       (vec (clojure.string/split
+              (clojure.string/replace string #"\t" "|") #"\|")))))
+
+;checking the first element of the resulting structure
+(nth plenary_register_mps-skl9_final 1)
+
+;2) ----------- parsing the ADDITIONAL file 'plenary_vote_results-skl9'
+(def plenary_vote_results-skl9_raw (vec (doall
+                 (line-seq
+                   (io/reader "../data files/plenary_vote_results-skl9.tsv")))))
+
+;writing the final structure into 'plenary_vote_results-skl9_final'
+(def plenary_vote_results-skl9_final (vec (for [string plenary_vote_results-skl9_raw]
+  (vec (clojure.string/split
+        (clojure.string/replace string #"\t" "|") #"\|")))))
+
+;checking the first element of the resulting structure
+(nth plenary_vote_results-skl9_final 1)
+
+;3) ----------- parsing the EXTRA file 'mps-declarations_rada'
 (def mps-declarations_rada (vec (with-open [reader (io/reader "../data files/mps-declarations_rada.json")]
-  (vector
-   (doall
-    (js/read reader))))))
-
-(vec
-  (vector
-     (clojure.string/split
-       (line-seq
-            (io/reader "../data files/plenary_vote_results-skl9.tsv"))
-       #"\\"
-        )))
+                                  (doall
+                                    (js/read reader)))))
